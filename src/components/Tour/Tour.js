61923ea3073e9554af6e40521c0e6607ae28f77b
Ajij_Khaan <ajijkhaan28@gmail.com>
2021-11-01 00:27:22 +0600

    update tours design

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -3,18 +3,22 @@ import { Card, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Package = (props) => {
-    const { _id, name, img, description } = props.tour;
+    const { _id, name, price, Duration, img } = props.tour;
     return (
         <div>
             <div className="">
                 <Col>
-                    <Card className="border-1 border-primary">
+                    <Card className="border-1 ">
                         <Card.Img variant="top" src={img} />
                         <Card.Body>
-                            <Card.Title>{name}</Card.Title>
-                            <Card.Text>
-                                {description.slice(0, 250)}
-                            </Card.Text>
+                            <Card.Title className='fs-5 fw-bold'>{name}</Card.Title>
+                            <div className='d-flex justify-content-center'>
+                                <div className='mx-3 d-flex justify-content-center'>
+                                    <p className='text-danger fw-bold fs-3'>${price}</p>
+                                    <p className='mt-2 text-muted fw-bold'>/ Per Person</p>
+                                </div>
+                                <p className='mx-3 text-muted fw-bold mt-2'>{Duration}</p>
+                            </div>
                             <Link to={`/booktour/${_id}`}> <button className="btn btn-primary ">Book Tour</button></Link>
                         </Card.Body>
                     </Card>
--- src/components/Tours/Tours.js
+++ src/components/Tours/Tours.js
@@ -13,7 +13,7 @@ const Package = () => {
     console.log(tours);
     return (
         <div className="bg-light" >
-            <h1 id="featured-services" className="font-body fw-bold mb-4 pt-5 fs-1">Our BEST TOURS</h1>
+            <h1 id="featured-services" className="font-body fw-bold mb-4 pt-5 fs-1">MOST POPULAR TOURS</h1>
             <Container className="mb-5">
                 <Row id="services" xs={1} md={2} lg={3} className="g-4 mt-5border-primary">
                     {
